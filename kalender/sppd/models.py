08c74c2c2a6fb9947fa5977aae91b73d1b57b4c8
muntaza <muhammad@muntaza.id>
2018-05-21 11:37:23 +0800

    OK

--- kalender/sppd/models.py
+++ kalender/sppd/models.py
@@ -6,7 +6,142 @@ from django.core.urlresolvers import reverse
 
 # Create your models here.
 
-SKPD
+class Provinsi(models.Model):
+    id = models.AutoField(primary_key=True, db_column="id")
+    kode_provinsi = models.CharField("Kode Provinsi", max_length=20,
+                    db_column="kode_provinsi")
+    nama_provinsi = models.CharField("Nama Provinsi", max_length=200,
+                    db_column="nama_provinsi")
+
+    class Meta:
+        db_table = "provinsi"
+        verbose_name = "Provinsi"
+        verbose_name_plural = "Provinsi"
+
+    def __unicode__(self):
+        return self.nama_provinsi
+
+
+class Kabupaten(models.Model):
+    id = models.AutoField(primary_key=True, db_column="id")
+    id_provinsi = models.ForeignKey(Provinsi, verbose_name="Provinsi",
+                    db_column="id_provinsi")
+    kode_kabupaten = models.CharField("Kode Kabupaten", max_length=20,
+                    db_column="kode_kabupaten")
+    nama_kabupaten = models.CharField("Nama Kabupaten", max_length=200,
+                    db_column="nama_kabupaten")
+
+    class Meta:
+        db_table = "kabupaten"
+        verbose_name = "Kabupaten"
+        verbose_name_plural = "Kabupaten"
+
+    def __unicode__(self):
+        return self.nama_kabupaten
+
+
+class LokasiBidang(models.Model):
+    id = models.AutoField(primary_key=True, db_column="id")
+    id_kabupaten = models.ForeignKey(Kabupaten, verbose_name="Kabupaten",
+                    db_column="id_kabupaten")
+    kode_lokasi_bidang = models.CharField("Kode Lokasi Bidang", max_length=20,
+                    db_column="kode_lokasi_bidang")
+    nama_lokasi_bidang = models.CharField("Nama Lokasi Bidang", max_length=200,
+                    db_column="nama_lokasi_bidang")
+
+    class Meta:
+        db_table = "lokasi_bidang"
+        verbose_name = "Lokasi Bidang"
+        verbose_name_plural = "Lokasi Bidang"
+
+    def __unicode__(self):
+        return "%s  %s" % (self.kode_lokasi_bidang, self.nama_lokasi_bidang)
+
+
+class SKPD(models.Model):
+    id = models.AutoField(primary_key=True, db_column="id")
+    id_lokasi_bidang = models.ForeignKey(LokasiBidang,
+                    verbose_name="Lokasi Bidang",
+                    db_column="id_lokasi_bidang")
+    kode_skpd = models.CharField("Kode SKPD", max_length=20,
+                    db_column="kode_skpd")
+    nama_skpd = models.CharField("Nama SKPD", max_length=200,
+                    db_column="nama_skpd")
+
+    class Meta:
+        db_table = "skpd"
+        verbose_name = "SKPD"
+        verbose_name_plural = "SKPD"
+
+    def __unicode__(self):
+        return self.nama_skpd
+
+
+
+class StatusPegawai(models.Model):
+    id = models.AutoField(primary_key=True, db_column="id")
+    status_pegawai = models.CharField("Status Pegawai", max_length=40,
+                    db_column="status_pegawai", unique=True)
+
+    class Meta:
+        db_table = "status_pegawai"
+        verbose_name = "Status Pegawai"
+        verbose_name_plural = "Status Pegawai"
+
+    def __unicode__(self):
+        return self.status_pegawai
+
+
+
+class Pegawai(models.Model):
+    id = models.AutoField(primary_key=True, db_column="id")
+    nama_pegawai = models.CharField("Nama Pegawai", max_length=200,
+                    db_column="nama_pegawai")
+    nip = models.CharField("NIP", max_length=60,
+                    db_column="nip")
+    id_status_pegawai = models.ForeignKey(StatusPegawai,
+                    verbose_name="Status Pegawai",
+                    db_column="id_status_pegawai")
+    id_skpd = models.ForeignKey(SKPD,
+                    verbose_name="SKPD",
+                    db_column="id_skpd")
+
+    class Meta:
+        db_table = "pegawai"
+        verbose_name = "Pegawai"
+        verbose_name_plural = "Pegawai"
+
+    def __unicode__(self):
+        return self.nama_pegawai
+
+
+
+class SPPD(models.Model):
+    id = models.AutoField(primary_key=True, verbose_name="No Urut",
+                    db_column="id")
+    nip = models.CharField("NIP", max_length=60,
+                    db_column="nip")
+    tanggal_berangkat = models.DateField("Tanggal Berangkat",
+                    db_column="tanggal_berangkat")
+    id_jenis_transaksi = models.ForeignKey(JenisTransaksi,
+                    verbose_name="Jenis Transaksi", null=True,
+                    db_column="id_jenis_transaksi")
+    id_sub_skpd = models.ForeignKey(SUBSKPD,
+                    verbose_name="SUB SKPD",
+                    db_column="id_sub_skpd")
+    keterangan = models.CharField("Keterangan", max_length=200,
+                    db_column="keterangan")
+
+    class Meta:
+        db_table = "transaksi"
+        verbose_name = "Transaksi"
+        verbose_name_plural = "Transaksi"
+
+    def __unicode__(self):
+        return "%s" % (self.id)
+
+
+
 #class Event(models.Model):
 #    day = models.DateField(u'Day of the event', help_text=u'Day of the event')
 #    start_time = models.TimeField(u'Starting time', help_text=u'Starting time')
